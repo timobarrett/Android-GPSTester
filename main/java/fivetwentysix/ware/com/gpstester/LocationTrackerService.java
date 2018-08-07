9c298f5c1a07315025f7eedcde37e23eb48ee07c
Tim Barrett <timobarrett@gmail.com>
2018-08-07 10:08:05 -0400

    Update LocationTrackerService.java
    Replaced deprecated location apis with current versions

--- main/java/fivetwentysix/ware/com/gpstester/LocationTrackerService.java
+++ main/java/fivetwentysix/ware/com/gpstester/LocationTrackerService.java
@@ -1,71 +1,117 @@
 package fivetwentysix.ware.com.gpstester;
 
+/************************************
+* Change Log
+*     7-Aug-2018 - replaced deprecated Location API with supported approach
+*
+*/
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
-import android.os.Bundle;
-import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
-import android.os.Message;
 import android.support.annotation.NonNull;
-import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
-public class LocationTrackerService extends Service implements
-        LocationListener,GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ResultCallback<Status> {
+public class LocationTrackerService extends Service {
     private static final String LOG_TAG = "gpstester- "+ LocationTrackerService.class.getSimpleName();
-    private Looper mServiceLooper;
-    private LocationRequest mLocationReq = null;
-    private GoogleApiClient mGoogleApiClient;
-
-
-
+    private FusedLocationProviderClient mFusedLocationClient;
+    private SettingsClient mSettingClient;
+    private LocationRequest mLocationRequest;
+    private LocationSettingsRequest mLocationSettingsRequest;
+    private LocationCallback mLocationCallback;
+    private Location mLocation;
+    private Boolean mRequestingLocationUpdates;
     public LocationTrackerService(){}
+
 
     @Override
     public void onCreate(){
         Log.d(LOG_TAG,"IN onCreate");
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mSettingClient = LocationServices.getSettingsClient(this);
+        mRequestingLocationUpdates = false;
+
+        setupLocationRequest();
+        buildLocationSettingsRequest();
+        createLocationCallback();
+        getLastLocation();
+        startLocationUpdates();
+
     }
 
-    @Override
-    public void onLocationChanged(Location location){
-        Log.d(LOG_TAG,"IN onLocationChanged");
-        if (location != null){
-            addDataToRepository(location);
+    private void createLocationCallback(){
+        Log.d(LOG_TAG,"IN createLocationCallback");
+        mLocationCallback = new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult){
+                Log.d(LOG_TAG,"IN onLocationResult");
+                super.onLocationResult(locationResult);
+                addDataToRepository(locationResult.getLastLocation());
+            }
+        };
+    }
+    /**
+     * setup the location request properties
+     *  smallest distance and balanced power caused issues
+     */
+    private void setupLocationRequest(){
+        Log.d(LOG_TAG,"IN setupLocationRequest");
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(Constants.LOCATION_INTERVAL_IN_MILLISECONDS);
+        mLocationRequest.setFastestInterval(Constants.FAST_LOCATION_INTERVAL);
+
+    }
+
+   private void buildLocationSettingsRequest(){
+        Log.d(LOG_TAG,"IN buildLocationSettingsRequest");
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        mLocationSettingsRequest = builder.build();
+   }
+
+    /** this works on a real phone
+     *
+     */
+    private void getLastLocation() {
+        Log.d(LOG_TAG,"IN getLastLocation");
+        try {
+            mFusedLocationClient.getLastLocation()
+                    .addOnCompleteListener(new OnCompleteListener<Location>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Location> task) {
+                            if (task.isSuccessful() && task.getResult() != null) {
+                               mLocation = task.getResult();
+                                Log.v(LOG_TAG,"GetLastLocation Success LAT = "+mLocation.getLatitude());
+                            } else {
+                                Log.v(LOG_TAG,"RESULT = "+task.getResult().toString());
+ //                               Log.w(LOG_TAG, "Failed to get location.");
+                            }
+                        }
+                    });
+        } catch (SecurityException unlikely) {
+            Log.e(LOG_TAG, "Lost location permission." + unlikely);
         }
     }
 
     private void addDataToRepository(Location location){
         Log.d(LOG_TAG,"IN addDataToRepository");
-        Intent intent = new Intent(Constants.DATA_UPDATE_BROADCAST);
-        intent.putExtra("LAT",location.getLatitude());
-        intent.putExtra("LON",location.getLongitude());
-        intent.putExtra("ACC",location.getAccuracy());
-        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
         LocationDataPoint locationDataPoint = new LocationDataPoint(location.getLatitude(),location.getLongitude(),location.getAccuracy());
         LocationDataStore.getInstance().setLocationDataStore(locationDataPoint);
     }
     @Override
     public int onStartCommand(Intent intent, int flags, int startId){
         Log.d(LOG_TAG,"IN onStartCommand");
-       // gp = intent.getParcelableExtra("GoogleObj");
-        Log.d(LOG_TAG, "BEFORE setupGoogleClientApi");
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-        mGoogleApiClient.connect();
         return START_STICKY;
     }
 
@@ -81,65 +127,27 @@ public class LocationTrackerService extends Service implements
         stopLocationUpdates();
         stopSelf();
     }
-    /**
-     * setup the location request properties
-     *  smallest distance and balanced power caused issues
-     */
-    void setupLocationRequest(){
-        Log.d(LOG_TAG,"IN setupLocationRequest");
-        mLocationReq = new LocationRequest();
-        mLocationReq.setInterval(Constants.LOCATION_INTERVAL_IN_MILLISECONDS);
-        mLocationReq.setFastestInterval(Constants.LOCATION_INTERVAL_IN_MILLISECONDS);
-        mLocationReq.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-    }
+
+
+    //implement the commented out parts in MainActivity / UI
     void startLocationUpdates(){
         Log.d(LOG_TAG,"IN startLocationUpdates");
         try {
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationReq, this);
-        }
-        catch(SecurityException se){
+                mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                        mLocationCallback, Looper.myLooper());
+
+        } catch(SecurityException se){
             Log.e(LOG_TAG,"Exception - "+ se.getMessage());
         }
     }
+
     /**
      * stop location updates - called
      */
     void stopLocationUpdates() {
-        // (http://developer.android.com/reference/com/google/android/gms/location/LocationListener.html).
         Log.d(LOG_TAG, "IN stopLocationUpdates");
-        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
     }
 
-    synchronized void setupGoogleClientApi(Context context) {
-
-        }
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult result) {
-        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
-        // onConnectionFailed.
-        Log.d(LOG_TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
-    }
-    /**
-     * called when connected status returned from google play services
-     * @param connectionHint - Bundle with connection information
-     */
-    @Override
-    public void onConnected(Bundle connectionHint) {
-        Log.d(LOG_TAG,"IN onConnected ");
-        setupLocationRequest();
-        startLocationUpdates();
-    }
-    /**
-     *
-     * @param status - status returned
-     */
-    public void onResult(@NonNull Status status){
-        Log.d(LOG_TAG,"onResult");
-    }
-    @Override
-    public void onConnectionSuspended(int cause) {
-        // The connection to Google Play services was lost for some reason. We call connect() to
-        Log.d(LOG_TAG,"GoogleApiClient Connection was suspended - cause = "+cause);
-    }
 
 }
